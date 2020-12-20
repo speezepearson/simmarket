3e559afab682f818c4a318aa75ee3d471ba9f5a3
Spencer Pearson <speeze.pearson+git@gmail.com>
2020-12-20 16:36:01 -0700

    add random seed arg, sanity checks, fix Xeno

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,23 @@ Try the above for both binding and non-binding floor (cap).
 
 // extern crate rand;
 use rand::Rng;
+use rand::rngs::StdRng;
 use rand::distributions::{Distribution, Uniform};
 use std::collections::HashMap;
 use std::cmp::{min, max};
+use rand::SeedableRng;
 
 fn main() {
+  let args: Vec<String> = std::env::args().collect();
+  let seed: u64 = args[1].parse::<u64>().unwrap();
+
+  let mut rng: StdRng = StdRng::seed_from_u64(seed);
+
   let mut agents = Vec::new();
 
   println!("setting up agent pool");
   for i in 0..1000 {
-    agents.push(Agent::new_random());
+    agents.push(Agent::new_random(&mut rng));
   }
 
   let mut assets = Vec::new();
@@ -41,9 +48,11 @@ fn main() {
   }
 
   execute_all_trades(&mut assets);
+
+  println!("done with main");
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 struct Agent {
     // Production ability per time unit of each commodity
     production_a: f64,
@@ -62,17 +71,16 @@ impl Agent {
     return self.consumption_a_coeff / self.consumption_b_coeff;
   }
 
-  fn new_random() -> Agent {
+  fn new_random(rng: &mut StdRng) -> Agent {
     let prod_dist = Uniform::new(0.0,1000.0);
     let coeff_dist = Uniform::new(0.0,1.0);
-    let mut rng = rand::thread_rng();
     
     return Agent {
-      production_a: prod_dist.sample(&mut rng),
-      production_b: prod_dist.sample(&mut rng),
+      production_a: prod_dist.sample(rng),
+      production_b: prod_dist.sample(rng),
 
-      consumption_a_coeff: coeff_dist.sample(&mut rng),
-      consumption_b_coeff: coeff_dist.sample(&mut rng),
+      consumption_a_coeff: coeff_dist.sample(rng),
+      consumption_b_coeff: coeff_dist.sample(rng),
     }
   }
 }
@@ -138,7 +146,7 @@ mod tests {
         buyer: 1,
         seller: 0,
         amount_a: 0.5,
-        price_per_a_in_b: 4.1,
+        amount_b: 0.12195121951219513,
       }
     );
 
@@ -150,13 +158,14 @@ mod tests {
         buyer: 1,
         seller: 0,
         amount_a: 0.5,
-        price_per_a_in_b: 4.1,
+        amount_b: 0.12195121951219513,
       }
     );
   }
 
 }
 
+#[derive(PartialEq, Debug, Copy, Clone)]
 struct Balance {
   a: f64,
   b: f64,
@@ -169,8 +178,8 @@ struct Trade {
   buyer: AgentId,
   seller: AgentId,
 
-  amount_a: f64,
-  price_per_a_in_b: f64,
+  amount_a: f64, // transferred from seller to buyer
+  amount_b: f64, // transferred from buyer to seller
 }
 
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
@@ -239,12 +248,23 @@ fn find_next_trade(assets : &Vec<(Agent, Balance)>) -> Option<Trade> {
   match (highest_bid, lowest_acceptable_ask) {
     (Some(bid), Some(ask)) => { 
       println!("matching bid {:?} against ask {:?}", bid, ask);
+      let (buyer, buyer_balance) = &assets[bid.agent_id];
+      let (seller, seller_balance) = &assets[ask.agent_id];
+      println!("  (balances: bidder {:?}, seller {:?})", buyer_balance, seller_balance);
       let clearing_price = (bid.price_per_a_in_b + ask.price_per_a_in_b) / 2.0;
+      let amount_a_buyer_can_afford = buyer_balance.b / clearing_price;
+      let (amount_a, amount_b) = if amount_a_buyer_can_afford < seller_balance.a {
+        // amount_a_buyer_can_afford is known to be < seller_balance.a due to the if
+        // statement above
+        (amount_a_buyer_can_afford, buyer_balance.b)
+      } else {
+        (seller_balance.a, clearing_price * seller_balance.a)
+      };
       return Some(Trade {
         buyer: bid.agent_id,
         seller: ask.agent_id,
-        amount_a: if bid.amount_a < ask.amount_a { bid.amount_a } else { ask.amount_a },
-        price_per_a_in_b: clearing_price,
+        amount_a: amount_a,
+        amount_b: amount_b,
       });
     }
     _ => { return None; }
@@ -252,21 +272,70 @@ fn find_next_trade(assets : &Vec<(Agent, Balance)>) -> Option<Trade> {
 }
 
 fn execute_one_trade(assets: &mut Vec<(Agent, Balance)>) -> bool /* done? */ {
+  println!("in execute_one_trade");
   match find_next_trade(assets) {
-    None => { return true; }
+    None => { 
+      println!("no more trades are possible");
+      return true;
+    }
     Some(trade) => {
       println!("executing {:?}", trade);
-      assets[trade.buyer] .1.a += trade.amount_a;
-      assets[trade.seller].1.a -= trade.amount_a;
-      assets[trade.buyer] .1.b -= trade.amount_a * trade.price_per_a_in_b;
-      assets[trade.seller].1.b += trade.amount_a * trade.price_per_a_in_b;
+      let (initial_buyer_utility, initial_seller_utility) = {
+        let (buyer, mut buyer_balance) = assets[trade.buyer];
+        let (seller, mut seller_balance) = assets[trade.seller];
+        (
+          buyer.utility(buyer_balance.a, buyer_balance.b),
+          seller.utility(seller_balance.a, seller_balance.b)
+        )
+      };
+      assets[trade.buyer] .1.a += trade.amount_a; if assets[trade.buyer] .1.a < 0.0 {panic!("oh no")}
+      assets[trade.seller].1.a -= trade.amount_a; if assets[trade.seller].1.a < 0.0 {panic!("oh no")}
+      assets[trade.buyer] .1.b -= trade.amount_b; if assets[trade.buyer] .1.b < 0.0 {panic!("oh no")}
+      assets[trade.seller].1.b += trade.amount_b; if assets[trade.seller].1.b < 0.0 {panic!("oh no")}
+      let (final_buyer_utility, final_seller_utility) = {
+        let (buyer, mut buyer_balance) = assets[trade.buyer];
+        let (seller, mut seller_balance) = assets[trade.seller];
+        (
+          buyer.utility(buyer_balance.a, buyer_balance.b),
+          seller.utility(seller_balance.a, seller_balance.b)
+        )
+      };
+      // println!("buyer {:?}", buyer);
+      // println!("  util {} -> {}", initial_buyer_utility, final_buyer_utility);
+      // println!("seller {:?}", seller);
+      // println!("  util {} -> {}", initial_seller_utility, final_seller_utility);
+      assert!(final_buyer_utility > initial_buyer_utility, "buyer's remorse");
+      assert!(final_seller_utility > initial_seller_utility, "seller's remorse");
       return false;
     }
   }
 }
 
 fn execute_all_trades(assets: &mut Vec<(Agent, Balance)>) {
-  while execute_one_trade(assets) {}
+  while !execute_one_trade(assets) {}
+  sanity_check_endpoint(assets);
 }
 
+fn sanity_check_endpoint(assets: &Vec<(Agent, Balance)>) {
+  let mut local = assets.clone();
+  local.sort_by(|(agent_1,_), (agent_2, _)| {
+    agent_1.indifference_price_of_a_in_b().partial_cmp(
+      &agent_2.indifference_price_of_a_in_b()
+    ).unwrap()
+  });
 
+  let remainder = local.iter()
+    .skip_while(|(_, balance)| {    balance.a == 0.0  })
+    .skip_while(|(_, balance)| {    balance.a > 0.0 && balance.b > 0.0  })
+    .skip_while(|(_, balance)| {    balance.b == 0.0  })
+    .collect::<Vec<_>>();
+  // println!("Agents:");
+  // for (agent, balance) in local.iter() {
+  //   println!("  ({}, {}, {}), {:?}", agent.indifference_price_of_a_in_b(), balance.a, balance.b, agent);
+  // }
+  // println!("Remainder:");
+  // for (agent, balance) in remainder.iter() {
+  //   println!("  ({}, {}, {}), {:?}", agent.indifference_price_of_a_in_b(), balance.a, balance.b, agent);
+  // }
+  assert!(remainder.is_empty(), "{:?} ({} elems)", remainder, remainder.len());
+}
